3ecdad90970bf6023d616424b49fb8f289e74713
luaroxy <luannamsiqueira@gmail.com>
2022-09-07 21:02:21 -0600

    Initial webpack and MVC setup

---
+++ src/scripts/controllers/mainController.js
@@ -0,0 +1,8 @@
+export default class MainController
+{
+    constructor(model, view)
+    {
+        this.model = model;
+        this.view = view;
+     }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/images', to: 'images/' }
+      ]
+    })
+
+  ]
+};
