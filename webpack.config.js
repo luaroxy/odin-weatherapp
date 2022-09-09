fa2a4cc57980c1a2cb6b8859fd6c4e4c9c5bd3e7
luaroxy <luannamsiqueira@gmail.com>
2022-09-08 18:51:25 -0600

    Added ESlint,Prettier, and Babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,49 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/scripts/index.js',
+  entry: "./src/scripts/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
       },
     ],
   },
   plugins: [
-
     new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './src/index.html',
-      filename: 'index.html'
+      inject: "body",
+      template: "./src/index.html",
+      filename: "index.html",
     }),
 
     new CopyWebpackPlugin({
-      patterns: [
-        { from: './src/images', to: 'images/' }
-      ]
-    })
-
-  ]
+      patterns: [{ from: "./src/images", to: "images/" }],
+    }),
+  ],
 };
