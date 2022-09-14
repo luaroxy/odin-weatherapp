2b43ac9246231e19abb7819a2c0dd13102e0427c
Sebastian Felling <sebastian@felling.de>
2022-09-14 16:38:59 +0200

    refactored APIs class

--- src/scripts/models/APIs.js
+++ src/scripts/models/APIs.js
@@ -1,9 +1,14 @@
 export default class APIs {
-  constructor() {}
+  constructor() {
+    // consider injecting the API key from a config file
+    this.urlGenerator = new UrlGenerator("e52320b984040185e6040a1e67f254e0");
+  }
 
   // eslint-disable-next-line class-methods-use-this
   async getGeoCoordinates(city) {
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&APPID=e52320b984040185e6040a1e67f254e0`;
+    const url = this.urlGenerator.generateGeoCoordsUrl(city);
+
+    // TODO: any idea how we can refactor these lines (see below) to avoid code duplication?
     const response = await fetch(url, { mode: "cors" });
     const geocodingData = await response.json();
 
@@ -14,6 +19,7 @@ export default class APIs {
 
   async getCurrentWeatherData(city, unit) {
     const { lat, lon } = await this.getGeoCoordinates(city);
+    // TODO: encapsulate these url templates inside the UrlGenerator class (see example above)
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e52320b984040185e6040a1e67f254e0&units=${unit}`;
     const response = await fetch(url, { mode: "cors" });
     const weatherData = await response.json();
@@ -23,10 +29,31 @@ export default class APIs {
 
   async getForecast(city, unit) {
     const { lat, lon } = await this.getGeoCoordinates(city);
+    // TODO: encapsulate these url templates inside the UrlGenerator class (see example above)
     const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=8&appid=e52320b984040185e6040a1e67f254e0&units=${unit}`;
     const response = await fetch(url, { mode: "cors" });
     const forecastData = await response.json();
     console.log(forecastData);
     return forecastData;
   }
+}
+
+/**
+ * Responsible for generating resource URLs against the constant base URL and a given API key.
+ * Generating URLs is a separate responsibility that should be kept outside of the APIs class.
+ */
+class UrlGenerator
+{
+  constructor(appId)
+  {
+    this.baseUrl = "https://api.openweathermap.org";
+    this.appId = appId;
+  }
+
+  generateGeoCoordsUrl(city)
+  {
+    return `${this.baseUrl}/geo/1.0/direct?q=${city}&appid=${this.appId}`;
+  }
+
+  // TODO: create methods for other URLs
 }
