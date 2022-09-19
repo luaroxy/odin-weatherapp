de321036294186318c0361f65d1053d6a665b936
luaroxy <luannamsiqueira@gmail.com>
2022-09-19 12:58:45 -0600

    Added search feature

--- src/scripts/controllers/mainController.js
+++ src/scripts/controllers/mainController.js
@@ -3,16 +3,22 @@ export default class MainController {
     this.model = model;
     this.view = view;
 
-    window.addEventListener("load", () => this.callFunc());
+    document.getElementById("search").addEventListener("blur", (e) => this.callFunc(document.getElementById("search").value));
+    document.getElementById("search").addEventListener("keypress", (e) => this.checkIfEnter(e));
+    window.addEventListener("load", () => this.callFunc("new york"));
   }
 
-  async callFunc() {
-    const cityInfo = await this.model.getCityInfo("Regina", "metric");
-    const currentWeather = await this.model.getCurrentWeather("Regina", "metric");
-    const forecastWeather = await this.model.getForecastWeather("Regina", "metric");
+  async callFunc(city) {
+    const cityInfo = await this.model.getCityInfo(city, "metric");
+    const currentWeather = await this.model.getCurrentWeather(city, "metric");
+    const forecastWeather = await this.model.getForecastWeather(city, "metric");
 
     this.view.appendCityInfo(cityInfo);
     this.view.appendCurrentWeather(currentWeather);
     this.view.appendForecastWeather(forecastWeather);
+  }
+
+  checkIfEnter(e) {
+    if (e.key === "Enter") document.getElementById("search").blur();
   }
 }
